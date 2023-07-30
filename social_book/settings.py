7f38ddaf1ca4112483424cbc633a3a599470154e
MikeMatovu <matovumike44@gmail.com>
2023-07-30 22:24:47 +0300

    Added Images to S3

--- social_book/settings.py
+++ social_book/settings.py
@@ -132,10 +132,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #aws settings
 
-# AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
+AWS_ACCESS_KEY_ID = 'YOUR ACCESS ID'
+AWS_SECRET_ACCESS_KEY = 'YOUR SECRET KEY'
+AWS_STORAGE_BUCKET_NAME = 'YOUR BUCKET NAME'
+AWS_S3_REGION_NAME = 'YOUR BUCKET REGION'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 # AWS_LOCATION = 'static'
 
 # STATICFILES_DIRS = [
@@ -148,7 +152,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AWS_LOCATION = 'media'  # Use 'media' instead of 'static' for media files
 
 # Use S3Boto3Storage for media files
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-# MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
-# MEDIA_ROOT = ''
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/'
+MEDIA_ROOT = ''
 
